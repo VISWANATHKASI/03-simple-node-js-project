4d043afeb8a4ddc09c0b283a7bc21b8c4eddccfe
VISWANATHKASI <100555681+VISWANATHKASI@users.noreply.github.com>
2025-11-23 23:42:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('** Hey Sis!!! my app was deployed :) **'); //write a response to the client
+  res.write('** Hey Bro!!! my app was deployed :) **'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
